4f1c4651a6247d49834aa3d47fe42c5866a998b9
SaiCharithaJosyula <75774903+SaiCharithaJosyula@users.noreply.github.com>
2020-12-30 20:37:05 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,11 @@
+# from webbrowser import Chrome
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# options = webdriver.ChromeOptions()
+#
+#
+# opts = Options()
+# opts.headless
+# assert opts.headless  # Operating in headless mode
+# browser = Chrome(options=opts)
+# browser.get('https://duckduckgo.com')
